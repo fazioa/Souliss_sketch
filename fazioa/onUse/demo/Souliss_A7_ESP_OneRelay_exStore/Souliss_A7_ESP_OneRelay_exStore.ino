8a0509220a526da25351aedaeb98eb47ff2bf5a0
Tonino <fazioa@gmail.com>
2016-09-10 17:13:56 +0200

    demo wifi peer ok

--- fazioa/onUse/demo/Souliss_A7_ESP_OneRelay_exStore/Souliss_A7_ESP_OneRelay_exStore.ino
+++ fazioa/onUse/demo/Souliss_A7_ESP_OneRelay_exStore/Souliss_A7_ESP_OneRelay_exStore.ino
@@ -32,6 +32,9 @@
 #include <EEPROM.h>
 #include "Souliss.h"
 
+#include <SPI.h>
+#include <OneWire.h>
+#include "DallasTemperature.h"
 //*************************************************************************
 // Define the network configuration according to your router settingsuration according to your router settings
 // and the other on the wireless oneless one
@@ -42,24 +45,36 @@
 //*************************************************************************
 
 #define SLOT_RELAY_0 0
-#define SLOT_RELAY_1 1
-#define PIN_SWITCH 14
+
+#define SLOT_TEMPERATURE_ONE        1     // This is the memory slot used for the execution of the logic in network_address1
+#define SLOT_TEMPERATURE_TWO     3
+
+//#define PIN_SWITCH 14
 #define PIN_RELAY_ON 12
 #define PIN_RELAY_OFF 13
 #define PIN_LED 2
+
+#define PIN_ONEWIRE_SENSORS 14
+
+#define DEADBAND        0.01    // Deadband value 1%
+#define DHTTYPE DHT22   // DHT 22 
+
+// Initialize DHT sensors
+OneWire oneWire(PIN_ONEWIRE_SENSORS);
+DallasTemperature sensors(&oneWire);
 
 void setup()
 {
   Serial.begin(9600);
   Initialize();
-   GetIPAddress();
-  
+  GetIPAddress();
+
   SetAddress(peer_address, myvNet_subnet, myvNet_supern);          // Address on the wireless interface
-  
+
   //*************************************************************************
   //*************************************************************************
 
-  pinMode(PIN_SWITCH, INPUT_PULLUP);    // Switch
+  //  pinMode(PIN_SWITCH, INPUT_PULLUP);    // Switch
 
   digitalWrite(PIN_RELAY_ON, LOW);
   pinMode(PIN_RELAY_ON, OUTPUT);    // Relay ON
@@ -68,12 +83,17 @@ void setup()
   pinMode(PIN_RELAY_OFF, OUTPUT);    // Relay OFF
   pinMode(PIN_LED, OUTPUT);
 
+
+  pinMode(PIN_ONEWIRE_SENSORS, INPUT);
+
   Set_SimpleLight(SLOT_RELAY_0);
-
-
+  Set_Temperature(SLOT_TEMPERATURE_ONE);
+  Set_Temperature(SLOT_TEMPERATURE_TWO);
 
   mOutput(SLOT_RELAY_0) = Souliss_T1n_OnCoil; //Set output to ON, then first execution of DigIn2State cause a change state to OFF.
 
+  // Start up the library
+  sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12.
 }
 
 void loop()
@@ -82,24 +102,47 @@ void loop()
     UPDATEFAST();
 
     FAST_2110ms() {
-    //  activity();
+      activity();
     }
 
     FAST_50ms() {
-      DigIn2State(PIN_SWITCH, Souliss_T1n_ToggleCmd, Souliss_T1n_ToggleCmd, SLOT_RELAY_0);
+      //    DigIn2State(PIN_SWITCH, Souliss_T1n_ToggleCmd, Souliss_T1n_ToggleCmd, SLOT_RELAY_0);
       Logic_SimpleLight(SLOT_RELAY_0);
-      Logic_SimpleLight(SLOT_RELAY_1);
-      Logic_SimpleLight(SLOT_RELAY_1);
+
       PulseLowDigOut(PIN_RELAY_ON, Souliss_T1n_OnCoil, SLOT_RELAY_0);
       PulseLowDigOut(PIN_RELAY_OFF, Souliss_T1n_OffCoil, SLOT_RELAY_0);
-
-      // Example for other optional relay
-      // Logic_SimpleLight(SLOT_RELAY_1);
-      // DigOut(PIN_RELAY_1, Souliss_T1n_Coil, SLOT_RELAY_1);
     }
     FAST_PeerComms();
   }
+
+  FAST_510ms() {
+    // Compare the acquired input with the stored one, send the new value to the
+    // user interface if the difference is greater than the deadband
+    Logic_Temperature(SLOT_TEMPERATURE_ONE);
+    Logic_Temperature(SLOT_TEMPERATURE_TWO);
+  }
+
+
+  FAST_2110ms() {
+    sensors.requestTemperatures(); // Send the command to get temperatures
+
+    float temperature = sensors.getTempCByIndex(0);
+    Serial.println(temperature);
+    ImportAnalog(SLOT_TEMPERATURE_ONE, &temperature);
+    temperature = sensors.getTempCByIndex(1);
+    Serial.println(temperature);
+    ImportAnalog(SLOT_TEMPERATURE_TWO, &temperature);
+
+  }
+
+  EXECUTESLOW() {
+    UPDATESLOW();
+    SLOW_10s() {
+      Timer_SimpleLight(SLOT_RELAY_0);
+    }
+  }
 }
+
 
 U8 activity_led_status = 0;
 void activity() {
