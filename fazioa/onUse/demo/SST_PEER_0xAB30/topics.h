3c9ab22632d20888873f6c9a962501b23e58c4b6
Tonino <fazioa@gmail.com>
2016-10-02 22:52:44 +0200

    SST

--- fazioa/onUse/demo/SST_PEER_0xAB30/topics.h
+++ fazioa/onUse/demo/SST_PEER_0xAB30/topics.h
@@ -9,8 +9,13 @@
 #define TOPIC5 0xF001,0x18
 #define TOPIC6 0xF001,0x0A
 
-//***************************************************** 
+#define SST_HEAT_ONOFF 0xF001,0x0B
+U8 HEAT_ON = 0X01;
+U8 HEAT_OFF = 0X00;
+
+//*****************************************************
 //  ES:
 //#define TEMPERATURE_TOPIC_NODE_DHT_RELE 0xF001,0x15
 //#define HUMIDITY_TOPIC_NODE_DHT_RELE 0xF001,0x16
- 
+
+
