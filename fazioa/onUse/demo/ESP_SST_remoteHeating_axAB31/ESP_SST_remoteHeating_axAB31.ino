a76ec070af835694d6f8a554815b129f3d5f977e
Tonino <fazioa@gmail.com>
2016-10-02 22:52:29 +0200

    ESP_SST_remoteHeating_axAB31

---
+++ fazioa/onUse/demo/ESP_SST_remoteHeating_axAB31/ESP_SST_remoteHeating_axAB31.ino
@@ -0,0 +1,72 @@
+#include "SoulissFramework.h"
+#include <ESP8266mDNS.h>
+#include <EEPROM.h>
+#include "bconf/MCU_ESP8266.h"              // Load the code directly on the ESP8266
+#include "conf/IPBroadcast.h"
+
+// **** Define the WiFi name and password ****
+#define WIFICONF_INSKETCH
+#define WiFi_SSID               "souliss"
+#define WiFi_Password           "souliss01"
+
+
+// Include framework code and libraries
+#include <ESP8266WiFi.h>
+#include "Souliss.h"
+#include "topics.h"
+//*************************************************************************
+// Define the network configuration according to your router settingsuration according to your router settings
+// and the other on the wireless oneless one
+#define peer_address  0xAB31
+#define myvNet_subnet 0xFF00
+#define myvNet_supern 0xAB10
+
+//*************************************************************************
+
+#define SLOT_POWERSOCKET 0
+#define PIN_POWERSOCKET 1
+
+uint8_t mypayload_len = 0;
+U8 mypayload;
+
+void setup()
+{
+  // Serial.begin(9600);
+  //  Serial.println("Start. Delay 10 seconds... ");
+  //delay 30 seconds
+  delay(10000);
+  Initialize();
+  //  Serial.print("Get IP Address: ");
+  GetIPAddress();
+  //Serial.println(WiFi.localIP());
+  SetAddress(peer_address, myvNet_subnet, myvNet_supern);          // Address on the wireless interface
+
+  // Define output pins
+  pinMode(PIN_POWERSOCKET, OUTPUT);    // Relè
+}
+
+void loop()
+{
+  EXECUTEFAST() {
+    UPDATEFAST();
+
+    FAST_50ms() {
+      Serial.print(".");
+      subcriptionHeating_ON_OFF();
+
+    }
+
+    FAST_PeerComms();
+  }
+}
+
+void subcriptionHeating_ON_OFF() {
+  if (subscribedata(SST_HEAT_ONOFF, &mypayload, &mypayload_len)) {
+    if (mypayload == HEAT_ON)
+      digitalWrite(PIN_POWERSOCKET, HIGH);
+    else
+      digitalWrite(PIN_POWERSOCKET, LOW);
+  }
+}
+
+
