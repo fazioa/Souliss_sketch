341d605407f67e8fb3a766339a1fa4ea210e41ce
Tonino <fazioa@gmail.com>
2017-05-17 00:26:55 +0200

    MOD OK

--- fazioa/LAB/tritasarde/tritasarde.ino
+++ fazioa/LAB/tritasarde/tritasarde.ino
@@ -5,83 +5,97 @@
 //Potenziometri consigliati: 10Kohms
 
 //COSTANTI
-const int MINTIMESECONDS=3;
-const int MAXTIMESECONDS=600;
+const long MINTIMESECONDS = 3;
+const long MAXTIMESECONDS = 240;
 
 
-#define MotionPotentiometerPin A0
-#define PausePotentiometerPin  A1
+#define MotionPotentiometerPin 14 //A0
+#define PausePotentiometerPin  15 //A1
 #define MotionLedPin 3
 #define PauseLedPin 4
 #define releOut 5
 
-unsigned long previousMillis = 0;
-unsigned long currentMillis=0;
+unsigned long previousSeconds = 0;
+unsigned long currentSeconds = 0;
 
-int MotionIntervalSeconds; //in seconds
-int PauseIntervalSeconds; //in seconds
+long MotionIntervalSeconds; //in seconds
+long PauseIntervalSeconds; //in seconds
 int tempVal;
-boolean bMotionState=true;
+boolean bMotionState = true;
 
 void setup()
 {
-	pinMode(PauseLedPin, OUTPUT);
-	pinMode(MotionLedPin, OUTPUT);
-	pinMode(PauseLedPin, OUTPUT);
-	pinMode(releOut, OUTPUT);
+  Serial.begin(9600);
+  pinMode(PauseLedPin, OUTPUT);
+  pinMode(MotionLedPin, OUTPUT);
+  pinMode(releOut, OUTPUT);
 
-	pinMode(MotionPotentiometerPin, INPUT);
-	pinMode(PausePotentiometerPin, INPUT);
-
+  pinMode(MotionPotentiometerPin, INPUT);
+  pinMode(PausePotentiometerPin, INPUT);
+  previousSeconds = millis() / 1000;
 }
 
-void stop()
+void stop_motor()
 {
-	digitalWrite(releOut, LOW);
-	digitalWrite(PauseLedPin, HIGH);
-	digitalWrite(MotionLedPin, LOW);
+  digitalWrite(releOut, LOW);
+  digitalWrite(PauseLedPin, HIGH);
+  digitalWrite(MotionLedPin, LOW);
 }
 
-void start()
+void start_motor()
 {
-	digitalWrite(releOut, HIGH);
-	digitalWrite(MotionLedPin, HIGH);
-	digitalWrite(PauseLedPin, LOW);
+  digitalWrite(releOut, HIGH);
+  digitalWrite(PauseLedPin, LOW);
+  digitalWrite(MotionLedPin, HIGH);
+
 }
 
 void loop()
 {
-	//read new values
-	tempVal= analogRead(MotionPotentiometerPin);
-	MotionIntervalSeconds= map(tempVal, 0, 1023, MINTIMESECONDS, MAXTIMESECONDS);
-	
-	tempVal = analogRead(PauseLedPin);
-	PauseIntervalSeconds= map(tempVal, 0, 1023, MINTIMESECONDS, MAXTIMESECONDS);
-	
-	if(bMotionState) {
-		//MOTION
-		start();
-		} else {
-		//PAUSE
-		stop();
-	}
+  //read new values
+  tempVal = analogRead(MotionPotentiometerPin);
+  MotionIntervalSeconds = map(tempVal, 0, 1023, MINTIMESECONDS, MAXTIMESECONDS);
+  Serial.print("Motion ");
+  Serial.print(MotionIntervalSeconds);
+  Serial.print("  -  ");
 
-	currentMillis = millis();
+  tempVal = analogRead(PausePotentiometerPin);
+  PauseIntervalSeconds = map(tempVal, 0, 1023, MINTIMESECONDS, MAXTIMESECONDS);
 
-	if(bMotionState) {
-		//MOTION - timer
-		if (currentMillis - previousMillis > MotionIntervalSeconds){
-			bMotionState=!bMotionState; //inverte stato moto / pausa
-			previousMillis = millis();
-		}
-		} else {
-		//PAUSE - timer
-		if (currentMillis - previousMillis > PausePotentiometerPin) {
-			bMotionState=!bMotionState; //inverte stato moto / pausa
-			previousMillis = millis();
-		}
-	}
-	
+  Serial.print("Pause ");
+  Serial.print(PauseIntervalSeconds);
+  Serial.print("  -  ");
+  Serial.print("bMotionState ");
+  Serial.print(bMotionState);
+
+  if (bMotionState) {
+    //MOTION
+    start_motor();
+  } else {
+    //PAUSE
+    stop_motor();
+  }
+
+  currentSeconds = millis() / 1000;
+
+  Serial.print("  -  ");
+  Serial.print("time ");
+  Serial.println(currentSeconds - previousSeconds);
+
+  if (bMotionState) {
+    //MOTION - timer
+    if (currentSeconds - previousSeconds > MotionIntervalSeconds) {
+      bMotionState = !bMotionState; //inverte stato moto / pausa
+      previousSeconds = millis() / 1000;
+    }
+  } else {
+    //PAUSE - timer
+    if (currentSeconds - previousSeconds > PauseIntervalSeconds) {
+      bMotionState = !bMotionState; //inverte stato moto / pausa
+      previousSeconds = millis() / 1000;
+    }
+  }
+
 }
 
 
