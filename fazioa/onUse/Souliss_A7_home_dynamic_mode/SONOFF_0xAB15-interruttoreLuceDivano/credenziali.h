837cffa698926de69165c2c5917c7ea06857f016
Tonino <fazioa@gmail.com>
2018-08-07 11:37:07 +0200

    -

--- fazioa/onUse/Souliss_A7_home_dynamic_mode/SONOFF_0xAB15-interruttoreLuceDivano/credenziali.h
+++
@@ -1,9 +0,0 @@
-// **** Define the WiFi name and password ****
-#define WIFICONF_INSKETCH
-#define WiFi_SSID               "asterix"
-#define WiFi_Password           "ttony2013"
-
-// **** Define Telegram parameters ****
-//#define   BOTTOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
-#define   BOTTOKEN "276333395:AAFIgSUpTI5F68c6Ei7I-LXBCJVrkWVnCvs"
-#define  CHAT_ID "48121590"
