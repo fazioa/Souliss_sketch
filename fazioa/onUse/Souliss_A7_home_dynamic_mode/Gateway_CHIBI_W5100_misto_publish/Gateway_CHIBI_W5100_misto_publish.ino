9495d098a8859b82c11d433ad81962be462491a8
Tonino <fazioa@gmail.com>
2018-09-17 23:51:28 +0200

    0xab17

--- fazioa/onUse/Souliss_A7_home_dynamic_mode/Gateway_CHIBI_W5100_misto_publish/Gateway_CHIBI_W5100_misto_publish.ino
+++ fazioa/onUse/Souliss_A7_home_dynamic_mode/Gateway_CHIBI_W5100_misto_publish/Gateway_CHIBI_W5100_misto_publish.ino
@@ -46,7 +46,7 @@ uint8_t ip_gateway[4]  = {192, 168, 1, 1};
 #define peer_wifi_address_LYT2  0xAB14 //LYT
 #define peer_wifi_address_SONOFF_Divano  0xAB15 //SONOFF Smart Socket - Divano
 #define peer_wifi_address_SONOFF_Cucina  0xAB16 //SONOFF Smart Socket - Cucina
-#define peer_wifi_address_SONOFF_Giardino  0xAB17 //SONOFF Smart Socket - Giardino
+#define peer_wifi_address_SONOFF_Giardino  0xAB18 //SONOFF Smart Socket - Giardino
 #define peer_wifi_address_termostato_soggiorno  0xAB20 //termostato soggiorno
 #define peer_wifi_address_termostato_piano_terra  0xAB21 //termostato piano terra
 #define peer_wifi_address_ex_store_birra  0xAB22 //nodo controllo temperatura birra
