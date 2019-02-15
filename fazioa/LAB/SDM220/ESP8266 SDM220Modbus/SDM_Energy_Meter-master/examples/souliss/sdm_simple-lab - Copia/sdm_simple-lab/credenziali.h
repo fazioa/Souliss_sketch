6a2a8fd02172c27201b6ca668d76a9ba7b74e080
Tonino <fazioa@gmail.com>
2019-02-15 20:58:03 +0100

    delete file

--- fazioa/LAB/SDM220/ESP8266 SDM220Modbus/SDM_Energy_Meter-master/examples/souliss/sdm_simple-lab - Copia/sdm_simple-lab/credenziali.h
+++
@@ -1,6 +0,0 @@
-#define WIFICONF_INSKETCH
-#define WiFi_SSID               "asterix"
-#define WiFi_Password           "ttony2013"
-
-#define   BOTTOKEN "276333395:AAFIgSUpTI5F68c6Ei7I-LXBCJVrkWVnCvs"
-#define  CHAT_ID "48121590" 
