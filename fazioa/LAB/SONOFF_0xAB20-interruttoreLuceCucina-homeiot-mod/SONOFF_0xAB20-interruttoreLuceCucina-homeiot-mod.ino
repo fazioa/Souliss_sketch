ffed14cec51d107be4427e0efedb05d1da35c490
Tonino <fazioa@gmail.com>
2020-04-09 18:20:33 +0200

    homeiot test

--- fazioa/LAB/SONOFF_0xAB20-interruttoreLuceCucina-homeiot-mod/SONOFF_0xAB20-interruttoreLuceCucina-homeiot-mod.ino
+++ fazioa/LAB/SONOFF_0xAB20-interruttoreLuceCucina-homeiot-mod/SONOFF_0xAB20-interruttoreLuceCucina-homeiot-mod.ino
@@ -1,13 +1,13 @@
 /**************************************************************************
-Interruttore luce cucina
-Funziona con switch collegato al pin 14
+  Interruttore luce cucina
+  Funziona con switch collegato al pin 14
 
   Sketch: POWER SOCKET - VER.2 - Souliss - Static Configuration
   Author: Tonino Fazio
-  
+
   ESP Core 2.3.0
   This example is only supported on ESP8266.
-  
+
   parametri upload Arduino IDE:
   – ESP8266 Generic
   – Flash Mode: DIO
@@ -43,18 +43,12 @@ Funziona con switch collegato al pin 14
 //#define WiFi_SSID               "SSID"
 //#define WiFi_Password           "PWD"
 
-// **** Define Telegram parameters ****
-//#define   BOTTOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
-//#define  CHAT_ID "chat ID number"  
-
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
 
 // Include framework code and libraries
 #include "Homie.h"
 #include "Souliss.h"
-
-
 
 //*************************************************************************
 // Define the network configuration according to your router settingsuration according to your router settings
@@ -91,7 +85,7 @@ void setup()
 
   digitalWrite(LED_BUILTIN, HIGH);
 
-  GetIPAddress(); 
+  GetIPAddress();
   Initialize();
 
   digitalWrite(LED_BUILTIN, LOW);
@@ -100,8 +94,8 @@ void setup()
   //*************************************************************************
   //*************************************************************************
   Set_SimpleLight(SLOT_POWERSOCKET);
-  Set_SimpleLight_mqtt_homie(SLOT_POWERSOCKET, "lucecucina");
-  
+  Set_SimpleLight_mqtt_homie(SLOT_POWERSOCKET, "lucecucina", "Luce Cucina");
+
   mOutput(SLOT_POWERSOCKET) = Souliss_T1n_OnCoil;
 
   Set_T51(T_WIFI_STRDB); //Imposto il tipico per contenere il segnale del Wifi in decibel
@@ -112,44 +106,47 @@ void setup()
   Serial.println(WiFi.macAddress());
   Serial.print("IP:  ");
   Serial.println(WiFi.localIP());
-  Serial.print("Subnet: ");
+  Serial.print("Subnet:  ");
   Serial.println(WiFi.subnetMask());
   Serial.print("Gateway: ");
-    Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.gatewayIP());
   Serial.println("Node Initialized");
 #endif
 
 
-// Setup MQTT subscription for onoff feed.
- // DA VEDERE MEGLIO
- //mqtt.subscribe(&MQTTrelay0_Read);
- 
- digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // Setup MQTT subscription for onoff feed.
+  // DA VEDERE MEGLIO
+  //mqtt.subscribe(&MQTTrelay0_Read);
+
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+//PUBLISH MESSAGE MQTT - DEVICE READY
+//Homie_device_ready();
 }
 
-int i=0;
+int i = 0;
 void loop()
 {
- 
+
 
 
   EXECUTEFAST() {
     UPDATEFAST();
 
- FAST_9110ms() {
+    FAST_9110ms() {
       // Ensure the connection to the MQTT server is alive (this will make the first
       // connection and automatically reconnect when disconnected).  See the MQTT_connect
       // function definition further below.
       MQTT_connect();
     }
 
- FAST_2110ms() {
+    FAST_2110ms() {
       //Processa le logiche per il segnale WiFi
       Read_T51(T_WIFI_STRDB);
       Read_T51(T_WIFI_STR);
     }
 
-    
+
 
     FAST_50ms() {
       DigIn2State(PIN_BUTTON_14, Souliss_T1n_ToggleCmd, Souliss_T1n_ToggleCmd, SLOT_POWERSOCKET);
@@ -173,41 +170,75 @@ void loop()
       //verifica ogni 90 sec (fast 91110) che la ESP sia collegata alla rete Wifi (5 tentativi al 6^fa hard reset)
       int tent = 0;
 
-                     while ((WiFi.status() != WL_CONNECTED) && tent < 4)
-                     {
-                     WiFi.disconnect();
-                     WiFi.mode(WIFI_STA);
-                     WiFi.begin(WiFi_SSID , WiFi_Password);
-                     int ritardo = 0;
-                     while ((WiFi.status() != WL_CONNECTED) && ritardo < 20)
-                     {
-                     delay(500);
-                     ritardo += 1;
+      while ((WiFi.status() != WL_CONNECTED) && tent < 4)
+      {
+        WiFi.disconnect();
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(WiFi_SSID , WiFi_Password);
+        int ritardo = 0;
+        while ((WiFi.status() != WL_CONNECTED) && ritardo < 20)
+        {
+          delay(500);
+          ritardo += 1;
 #ifdef SERIAL_DEBUG
-                     Serial.println(ritardo);
+          Serial.println(ritardo);
 #endif
-                   }
+        }
 
-                     if (WiFi.status() != WL_CONNECTED )
-                     delay(2000);
-                     tent += 1;
-                   }
+        if (WiFi.status() != WL_CONNECTED )
+          delay(2000);
+        tent += 1;
+      }
 #ifdef SERIAL_DEBUG
-                     if (tent > 4)Serial.println("tentativo non riuscito");
+      if (tent > 4)Serial.println("tentativo non riuscito");
 #endif
-                   }
+    }
 
-                     FAST_PeerComms();
-                   }
+    FAST_PeerComms();
+  }
 
-                     EXECUTESLOW() {
-                     UPDATESLOW();
-                     SLOW_10s() {  // Process the timer every 10 seconds
-                     Timer_SimpleLight(SLOT_POWERSOCKET);
-                   }
-                    SLOW_70s() {
-                     Homie_init();
-                   }
-                   }
+  EXECUTESLOW() {
+    UPDATESLOW();
+    SLOW_10s() {  // Process the timer every 10 seconds
+      Timer_SimpleLight(SLOT_POWERSOCKET);
+    }
+    SLOW_70s() {
+      Homie_init();
+    }
+  }
 
-                   }
+}
+void check_wifi_signal() {
+  long rssi = WiFi.RSSI();
+  int bars = 0;
+
+  if (rssi > -55) {
+    bars = 5;
+  }
+  else if (rssi < -55 & rssi > -65) {
+    bars = 4;
+  }
+  else if (rssi < -65 & rssi > -70) {
+    bars = 3;
+  }
+  else if (rssi < -70 & rssi > -78) {
+    bars = 2;
+  }
+  else if (rssi < -78 & rssi > -82) {
+    bars = 1;
+  }
+  else {
+    bars = 0;
+  }
+  float f_rssi = (float)rssi;
+  float f_bars = (float)bars;
+  ImportAnalog(T_WIFI_STRDB, &f_rssi);
+  ImportAnalog(T_WIFI_STR, &f_bars);
+
+#ifdef SERIAL_DEBUG
+  Serial.print("wifi rssi: ");
+  Serial.println(f_rssi);
+  Serial.print("wifi bars: ");
+  Serial.println(f_bars);
+#endif
+}
