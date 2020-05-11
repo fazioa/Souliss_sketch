214beca6918c5305195bdd43353be11d1a698203
fazioa <fazioa@gmail.com>
2020-05-11 11:28:47 +0200

    -

--- fazioa/onUse/Souliss_A7_home_dynamic_mode/SONOFF_0xAB17-interruttoreMuroForno/credenziali.h
+++
@@ -1,6 +0,0 @@
-#define WIFICONF_INSKETCH
-#define WiFi_SSID               "****"
-#define WiFi_Password           "****"
-
-#define  BOTTOKEN "XXxxxx:XXXXXXXXXXX-XXXXXXX"
-#define  CHAT_ID "123450" 
